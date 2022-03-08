62e52222c826c4eb2bbfed23cc1155858c5b7988
권은서 <kwonenseo@gmail.com>
2022-03-08 20:01:51 +0900

    Modify: repeat codes to function

--- main.js
+++ main.js
@@ -29,17 +29,28 @@ waterEl.querySelector("span").innerHTML = `${waterPrice}원`;
 coffeeEl.querySelector("span").innerHTML = `${coffeePrice}원`;
 
 cash.addEventListener("click", ()=>{
-  pay = "cash";
-  sectionCash.style.display = "block";
-  sectionCard.style.display = "none";
+  if (pay==="card" && cardMoney !== 0) {
+    alert("구매 종료 후 진행해주세요");
+  } else {
+    pay = "cash";
+    sectionCash.style.display = "block";
+    sectionCard.style.display = "none";
+  }
 })
 card.addEventListener("click", ()=>{
-  pay = "card";
-  buyListEl.querySelector("#changeBtn").innerHTML = "구매 종료";
-  sectionCash.style.display = "none";
-  sectionCard.style.display = "block";
-  cardMoney = prompt("고객님의 카드 잔액은?");
-  sectionCard.querySelector("p").innerHTML = `카드 잔액 : ${cardMoney}`;
+  if(pay === "cash" && money !== 0){
+    alert("구매 종료 후 진행해주세요");
+  } else {
+    pay = "card";
+    sectionSelectDrink.style.display ="none";
+    sectionCash.style.display = "none";
+    sectionCard.style.display = "block";
+    cardMoney = prompt("고객님의 카드 잔액은?");
+    while(isNaN(cardMoney)){
+      cardMoney = prompt("다시 입력해주세요! 고객님의 카드 잔액은?");
+    }
+    sectionCard.querySelector("p").innerHTML = `카드 잔액 : ${cardMoney}원`;
+  }
 })
 
 cashInputBtn.addEventListener("click", () => {
@@ -47,100 +58,62 @@ cashInputBtn.addEventListener("click", () => {
 })
 
 cokeEl.addEventListener("click", ()=>{
-  if(pay === "cash" && money >= cokePrice) {
-
-    alert("cash 콜라 살 수 있음~");
-    cokeCount += 1;
-    buyListEl.querySelector("ul>li:first-child>span").innerHTML = `${cokeCount}개`;
-    money -= cokePrice;
-    change.innerHTML = `잔액 : ${money}원`;
-
-  } else if (pay === "cash" && money <= cokePrice) {
-
-    alert ("잔액 부족! 현금을 더 투입하세요.");
-
-  } else if (pay === "card" && cardMoney >= cokePrice) {
-
-    alert("card 콜라~");
-    cokeCount += 1;
-    buyListEl.querySelector("ul>li:first-child>span").innerHTML = `${cokeCount}개`;
-    cardMoney -= cokePrice;
-    sectionCard.querySelector("p").innerHTML = `카드 잔액 : ${cardMoney}원`;
-
-  } else if (pay === "card" && cardMoney <= cokePrice) {
-
-    alert ("잔액이 부족합니다.")
-
-  } else {
-
-    alert("결제수단을 먼저 선택해주세요.");
-
-  }
+  cokeCount = SelectDrink("coke", cokePrice, cokeCount, "first-child");
 })
+
 waterEl.addEventListener("click", ()=>{
-  if(pay === "cash" && money >= waterPrice) {
-
-    alert("cash 물 살 수 있음~");
-    waterCount += 1;
-    buyListEl.querySelector("ul>li:nth-child(2)>span").innerHTML = `${waterCount}개`;
-    money -= waterPrice;
-    change.innerHTML = `잔액 : ${money}원`;
-
-  } else if (pay === "cash" && money <= waterPrice) {
-
-    alert("잔액 부족! 현금을 더 투입하세요.");
-
-  } else if (pay === "card" && cardMoney >= waterPrice) {
-
-    alert("card 물~");
-    waterCount += 1;
-    buyListEl.querySelector("ul>li:nth-child(2)>span").innerHTML = `${waterCount}개`;
-    cardMoney -= waterPrice;
-    sectionCard.querySelector("p").innerHTML = `카드 잔액 : ${cardMoney}원`;
-
-  } else if (pay === "card" && cardMoney <= waterPrice) {
-
-    alert ("잔액이 부족합니다.")
-
-  } else {
-
-    alert("결제수단을 먼저 선택해주세요.");
-
-  }
+  waterCount = SelectDrink("water", waterPrice, waterCount, "nth-child(2)");
 })
+
 coffeeEl.addEventListener("click", ()=>{
-  if(pay === "cash" && money >= coffeePrice) {
-
-    alert("cash 커피 살 수 있음~");
-    coffeeCount += 1;
-    buyListEl.querySelector("ul>li:last-child>span").innerHTML = `${coffeeCount}개`;
-    money -= coffeePrice;
-    change.innerHTML = `잔액 : ${money}원`;
-
-  } else if (pay === "cash" && money <= coffeePrice) {
-
-    alert("잔액 부족! 현금을 더 투입하세요.");
-
-  } else if (pay === "card" && cardMoney >= coffeePrice) {
-
-    alert("card 커피~");
-    coffeeCount += 1;
-    buyListEl.querySelector("ul>li:last-child>span").innerHTML = `${coffeeCount}개`;
-    cardMoney -= coffeePrice;
-    sectionCard.querySelector("p").innerHTML = `카드 잔액 : ${cardMoney}원`;
-
-  } else if (pay === "card" && cardMoney <= coffeePrice) {
-
-    alert ("잔액이 부족합니다.")
-
-  } else {
-
-    alert("결제수단을 먼저 선택해주세요.");
-
-  }
+  coffeeCount = SelectDrink("coffee", coffeePrice, coffeeCount, "last-child");
 })
 
 buyListEl.querySelector("#changeBtn").addEventListener("click", () => {
+  BuyingDone();
+})
+
+function SelectDrink(drink, price,count,child) {
+  if(pay === "cash" && money >= price) {
+    alert(`${drink} 나왔습니다!`);
+    count += 1;
+    buyListEl.querySelector(`ul>li:${child}>span`).innerHTML = `${count}개`;
+    money -= price;
+    change.innerHTML = `잔액 : ${money}원`;
+    
+  } 
+  else if (pay === "cash" && money <= price) {
+    alert("잔액 부족! 현금을 더 투입하세요.");
+    
+  } 
+  else if (pay === "card" && cardMoney >= price) {
+    alert(`${drink} 나왔습니다!`);
+    count += 1;
+    buyListEl.querySelector(`ul>li:${child}>span`).innerHTML = `${count}개`;
+    cardMoney -= price;
+    sectionCard.querySelector("p").innerHTML = `카드 잔액 : ${cardMoney}원`;
+    
+  } 
+  else if (pay === "card" && cardMoney <= price) {
+    alert ("잔액이 부족합니다.")
+  } 
+  else {
+    alert("결제수단을 먼저 선택해주세요.");
+  }
+  return count;
+}
+
+
+const BuyingDone = () => {
+  alert(`구입 내역 : 콜라 ${cokeCount}개 / 물 ${waterCount}개 / 커피 ${coffeeCount}개`);
+
+  cokeCount = 0;
+  waterCount = 0;
+  coffeeCount = 0;
+  buyListEl.querySelectorAll("span").forEach((current) => {
+    current.innerHTML = "0개";
+  })
+
   if(pay === "cash" && money===0){
     alert("반환할 잔돈이 없습니다!")
   } else if(pay === "cash" && money!==0) {
@@ -148,28 +121,36 @@ buyListEl.querySelector("#changeBtn").addEventListener("click", () => {
     money = 0;
     change.innerHTML = `잔액 :  ${money}원`;
   } else if (pay === "card") {
-    alert("맛있게 드세요~~~:)");
+    alert("맛있게 드세요~~~ :)");
     cardMoney = 0;
     sectionCard.querySelector("p").innerHTML = `카드 잔액 : ${cardMoney}원`;
   }
-  
-})
+  pay=null;
+}
+
 
 const PrintInput = () => {
-  money += parseInt(cashInput.value);
-
-  if(money % 100 <= 99 && money % 100 >= 1){
-    alert("10원 단위는 받지 못합니다...");
-    let payBack = money % 100;
-    money = money - payBack;
-    alert(`돌려드린 금액 : ${payBack}원 / 넣으신 금액 : ${money}원`);
+  if (isNaN( parseInt(cashInput.value) )) {
+    alert("다시 입력해주세요!");
     cashInput.value="";
   } else {
-    alert("넣으신 금액 : " + money);
-    cashInput.value="";
+    money += parseInt(cashInput.value);
+
+    if(money % 100 <= 99 && money % 100 >= 1){
+      alert("1원, 10원 단위는 받지 못합니다...");
+      let payBack = money % 100;
+      money = money - payBack;
+      alert(`돌려드린 금액 : ${payBack}원 / 넣으신 금액 : ${money}원`);
+      cashInput.value="";
+    } else {
+      alert("넣으신 금액 : " + money);
+      cashInput.value="";
+    }
+    change.innerHTML = `잔액 : ${money}원`;
+    sectionSelectDrink.style.display ="block";
   }
-  change.innerHTML = `잔액 : ${money}원`;
-  sectionSelectDrink.style.display ="block";
+
+  
 }
 
 
